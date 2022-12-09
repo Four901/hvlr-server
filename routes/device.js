a3046ed4ee1be6047e5fc34c0b70fb94b4a79df7
Ashish Meena <akm411201@gmail.com>
2022-12-09 19:43:09 +0530

    image 5

--- routes/device.js
+++ routes/device.js
@@ -187,7 +187,7 @@ router.put('/updateimage',fetchUser1,[
 
                 for(let j=0;j<images.length;i++)
                 {
-                  newImage[i]=Number.parseInt(images[i]);
+                  newImage.push(Number.parseInt(images[i]))
                 }
                 console.log(newImage)
                let arr= new Uint16Array(newImage).buffer;
