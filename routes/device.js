bb5e0ee473c6093f8651f38457607d8a794b47e2
Ashish Meena <akm411201@gmail.com>
2022-12-09 23:35:28 +0530

    image18

--- routes/device.js
+++ routes/device.js
@@ -315,6 +315,7 @@ router.put('/controllingside',fetchUser1,[
                 let device=await Device.find({user:req.user.id,Number:number})
                 
                 if(!device){return res.status(404).send("Not Found")}
+                console.log(device)
                 console.log("device"+" "+device.user)
                 console.log(req.user.id)
                 if(device.user.toString()!==req.user.id){return res.status(404).send("Not Allowed")}
