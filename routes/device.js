ec51b2907d68e3f46a1ec389d7344d0fc884a578
Ashish Meena <akm411201@gmail.com>
2022-12-09 22:47:54 +0530

    image11

--- routes/device.js
+++ routes/device.js
@@ -285,6 +285,78 @@ router.get('/getdevices/',fetchUser,[
              res.status(500).send("Some error occured")
          }
             })
+
+
+
+router.put('/controllingside',fetchUser1,[
+    
+
+        
+            ],async (req,res)=>{
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                  return res.status(400).json({ errors: errors.array() });
+                }
+                try{
+                   // console.log(req)
+                const number=parseInt(req.headers.number);
+                const status=req.headers.status;
+               
+    
+                console.log(status.toString()+" "+number)
+               
+                const newDevice={}
+              
+                
+                if(status.toString()=="online"){newDevice.status=status.toString()}
+                
+         
+         
+                let device=await Device.find({user:req.user.id,Number:number})
+                if(!device){return res.status(404).send("Not Found")}
+                if(device.user.toString()!==req.user.id){return res.status(404).send("Not Allowed")}
+                 
+               // if(newReply.question==null)newReply.question=post.question
+                if(newDevice.D0==null)newDevice.D0=device.D0
+                if(newDevice.D1==null)newDevice.D1=device.D1
+                if(newDevice.D2==null)newDevice.D2=device.D2
+                if(newDevice.D3==null)newDevice.D3=device.D3
+                if(newDevice.D4==null)newDevice.D4=device.D4
+                if(newDevice.D5==null)newDevice.D5=device.D5
+                if(newDevice.D6==null)newDevice.D6=device.D6
+                if(newDevice.D7==null)newDevice.D7=device.D7
+                if(newDevice.D8==null)newDevice.D8=device.D8
+                if(newDevice.status==null)newDevice.status=device.status
+                if(newDevice.image==null)newDevice.image=device.image
+                if(newDevice.camStatus==null)newDevice.camStatus=device.camStatus
+                if(newDevice.Number==null)newDevice.Number=device.Number
+                
+                device=await Device.find({user:req.user.id,Number:number}).update(newDevice)
+              
+                let sendDevice=[];
+               
+                sendDevice.push(newDevice._id);
+                sendDevice.push(newDevice.D0);
+                sendDevice.push(newDevice.D1);
+                sendDevice.push(newDevice.D2);
+                sendDevice.push(newDevice.D3);
+                sendDevice.push(newDevice.D4);
+                sendDevice.push(newDevice.D5);
+                sendDevice.push(newDevice.D6);
+                sendDevice.push(newDevice.D7);
+                sendDevice.push(newDevice.D8);
+
+                
+                res.json(sendDevice)
+                 }
+                 catch(error)
+             {
+                 console.error(error.message)
+                 res.status(500).send("Some error occured")
+             }
+                })
+
+
 router.get('/getdevice',fetchUser1,async (req,res)=>{
              
                 const errors = validationResult(req);
