e2092206515f42f07c4169f34fbb0142dcd9ba33
Ashish Meena <akm411201@gmail.com>
2022-12-15 23:09:52 +0530

    done3

--- middleware/fetchUser1.js
+++ middleware/fetchUser1.js
@@ -11,7 +11,7 @@ const JWT_KEY=process.env.jwt_key
 const fetchUser1=async(req,res,next)=>{
     
     console.log("i am fetching")
-    console.log(headers)
+    console.log(req.headers)
     let token=req.headers.authtoken;
     console.log("auth")
     console.log(token)
